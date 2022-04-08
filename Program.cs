700ef5247a7338d5405ec2b13087adcd27ab688c
Philip Graham <GrahPhil963@GoTVDSB.ca>
2022-04-08 10:11:33 -0400

    I worked really hard on my project

--- Program.cs
+++ Program.cs
@@ -10,8 +10,54 @@ namespace Part_6_Assignment_PHILIP_GRAHAM
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Whats up dude");
+            //Prompter
+            double minNumber;
+            double maxNumber;
+            double numberInput;
+            Console.WriteLine("Please enter a minimum number");
+            while (!Double.TryParse(Console.ReadLine(), out minNumber))
+                Console.WriteLine("Just follow the instructions man enter a number ");
+            Console.WriteLine("Please enter a maximum value");
+            do
+            {
+                while (!Double.TryParse(Console.ReadLine(), out maxNumber))
+                    Console.WriteLine("Do you not understand?? just enter a maximum number");
+                if (maxNumber < minNumber)
+
+                    Console.WriteLine("The maximum number needs to be more than the minimum");
+            } while (maxNumber <= minNumber);
+            
+
+           
+
+            do 
+            {
+                Console.WriteLine($"Please Enter a number between {minNumber} and {maxNumber}  ");
+                while (!Double.TryParse(Console.ReadLine(), out numberInput)) ;
+
+                
+            }while(numberInput <= minNumber||numberInput >= maxNumber);
+            Console.WriteLine("Well done sir");
+            Console.ReadLine();
+
+
+             
+               
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         }
+        
     }
 }
